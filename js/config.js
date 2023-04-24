ebe8ff8584eb8d4b4d3d54fc274034c2ae8558e5
JofielG <113305796+JofielG@users.noreply.github.com>
2023-04-23 21:39:43 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-1_anNbY6kNN',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '269k8c9iq0so199l2enrnj1ugq',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         'us-east-1_nLOb2PGcD',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '4i0kqa8naa42ete0j14m19cpvb',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://lpjealuyn3.execute-api.us-east-1.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
